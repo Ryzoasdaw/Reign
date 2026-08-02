8997ba5b191e37be4e3c9a7efc2454a06ca59651
naw1 <safahtor@gmail.com>
2026-08-03 02:32:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -332,7 +332,7 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
             const parentCategory = process.env.CATEGORY_ID || null;
 
             const tempVoiceChannel = await guild.channels.create({
-                name: ` | ${member.user.username}`,
+                name: ` | ${member.displayName}`, // تم التعديل هنا لاستخدام النك نيم (Nickname)
                 type: ChannelType.GuildVoice,
                 parent: parentCategory,
                 permissionOverwrites: [
